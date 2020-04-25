950a9d47d1e1c8c4fb211f76cbd5421cd7d1724f
alexhamcuco <whitebookopen@gmail.com>
2020-04-25 21:49:50 +0200

    color, contact page

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-
 const express = require('express');
 const app = express();
 const path = require('path');
 
 // settings
-app.set('port', 4000);
+app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 //middlwares
-
-
 
 // routes
 app.use(require('./routes/index'));
 // static files
-
 
 // listening the server
 app.listen(app.get('port'), () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,12 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
-    res.render('index', { title: 'Spanish with Alex' });
+router.get("/", (req, res) => {
+  res.render("index", { title: "Spanish with Alex" });
 });
 
-router.get('/contact', (req, res) => {
-
-    res.render('contact', { title: 'pagina de contacto de mierda' });
+router.get("/contact", (req, res) => {
+  res.render("contact", { title: "pagina de contacto de mierda" });
 });
 
 module.exports = router;
