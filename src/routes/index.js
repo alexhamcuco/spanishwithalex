e0f8cd49457e00b135414decf506cc88164677a3
alexhamcuco <whitebookopen@gmail.com>
2020-04-26 23:26:04 +0200

    footer logo problem

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index", { title: "Spanish with Alex" });
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Spanish with Alex' });
 });
 
-router.get("/contact", (req, res) => {
-  res.render("contact", { title: "pagina de contacto de mierda" });
+router.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Contact me' });
+});
+
+router.get('/materials', (req, res) => {
+    res.render('materials', { title: 'Materials to learn' });
 });
 
 module.exports = router;
