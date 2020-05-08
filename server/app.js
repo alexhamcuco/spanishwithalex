4c8caad02cdc8dd3ac01001fff31731e8aa05acc
Matthew <mjk276@cornell.edu>
2020-05-08 17:06:41 -0400

    checking in new structure

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// settings
+app.set('port', 3000);
+// set the starting path for the views
+// view engine setup
+app.set('views', path.join('./client/pages'));
+app.set('view engine', 'ejs');
+
+//middlwares
+
+// routes
+app.use(require('./routes/controller'));
+
+// set path for static assets
+app.use(express.static(path.join('./client/content')));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(__dirname + '/images'));
+// app.use('/content', express.static(__dirname + '/content')); //allow content folder to be used
+
+// listening the server
+app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'));
+});
