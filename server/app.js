aed8ba0dd933f396a878ef3ec81fcf18c30691b2
alexhamcuco <whitebookopen@gmail.com>
2020-05-12 12:33:22 +0200

    failing to set up mailing
    it shows only brackets on terminal, not logging details from form

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const path = require('path');
 var hbs = require('express-handlebars');
 
@@ -18,6 +20,14 @@ app.engine(
     })
 );
 
+// Body Parser Middlewre
+// parser application /x-www-form-urlencoded
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parser application/json
+app.use(bodyParser.json());
+
 // //if you configure Exprees to look for views in a custom location, y'll need to felect that by passin an updated
 // path as teh layoutsDir porperty in your configuracion. partialsDir="views/partials/"
 app.set('views', `./client/pages`);
@@ -33,6 +43,15 @@ app.use(express.static('./client/content'));
 // app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static(__dirname + '/images'));
 // app.use('/content', express.static(__dirname + '/content')); //allow content folder to be used
+
+//mailing set up, porst=route submition
+app.get('/', (req, res) => {
+    res.render('contact');
+});
+
+app.post('/send', (req, res) => {
+    console.log(req.body);
+});
 
 // listening the server
 app.listen(app.get('port'), () => {
